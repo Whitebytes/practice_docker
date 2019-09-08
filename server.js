f8011388d60f0e8a8021907fe9b98f24f74472fc
geertjan <gjkemme@gmail.com>
2019-09-08 12:13:26 +0200

    ficed ports

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const server = express();
-const PORT = 3000;
-const pg = require('pg');
-pg.connect('postgres://postgres:password@localhost:5432/practicedocker');
+const DBPORT = process.env.PORT || 5431;
+const HOST = process.env.HOST || 'localhost';
+const PORT =3000;
+
+const { Client } = require('pg')
+
+const client = new Client({
+    host: HOST,
+    port: DBPORT,
+    user: 'postgres',
+    database: 'postgres',
+    password: 'password',
+  })
+console.log(DBPORT, HOST)
+client.connect();
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
 server.get('/', (req, res) => res.status(200).send('hello'));
